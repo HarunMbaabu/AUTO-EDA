362968918fe65c4ee588797af789a35096d8dd1a
Harun Mbaabu <51136314+HarunMbaabu@users.noreply.github.com>
2023-05-08 14:54:00 +0300

    Add code to read parquet

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def load_dataset(file_path):
         return pd.read_csv(file_path)
     elif file_path.endswith('.json'):
         return pd.read_json(file_path)
+    elif file_path.endswith('.json'):
+        return pd.read_parquet(file_path)
     elif file_path.endswith('.xlsx'):
         return pd.read_excel(file_path)
 
